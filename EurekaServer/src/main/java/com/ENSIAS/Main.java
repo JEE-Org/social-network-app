4ebdecdbb536c1487ca005bc9efa4749b130228f
Haitam El Hayani <haitamelhayanitech@gmail.com>
2023-04-21 09:31:55 +0000

    Eureka server confing at application.properties

--- EurekaServer/src/main/java/com/ENSIAS/Main.java
+++ EurekaServer/src/main/java/com/ENSIAS/Main.java
@@ -1,7 +1,14 @@
 package com.ENSIAS;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
