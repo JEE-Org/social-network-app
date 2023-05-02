eefd83e2fd085d9d9996bf90116254242590bdbc
Haitam El Hayani <haitamelhayanitech@gmail.com>
2023-05-02 02:20:47 +0100

    CRegistration Cntroller updatesÃ

--- RegistrationService/src/main/java/com/ENSIAS/controller/ENSIAStController.java
+++ RegistrationService/src/main/java/com/ENSIAS/controller/ENSIAStController.java
@@ -6,29 +6,39 @@ import com.ENSIAS.model.LoginRequest;
 import com.ENSIAS.model.RegistrationRequest;
 import com.ENSIAS.service.ENSIAStService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/ENSIASts")
 @AllArgsConstructor
 public class ENSIAStController {
 
     private final ENSIAStService ensiaStService;
-    @GetMapping
+    @GetMapping("/home")
     public String get(){
         return "hello world";
     }
 
     @PostMapping("/signup")
-    public void registerENSIASt(@RequestBody RegistrationRequest request){
-        ensiaStService.registerENSIASt(request);
+    public ResponseEntity<String> registerENSIASt(@RequestBody RegistrationRequest request){
+        ENSIASt ensiaSt = ensiaStService.registerENSIASt(request);
+        if(ensiaSt==null){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("This email is already registered");
+        }
+        return ResponseEntity.ok("ENSIASt created");
     }
 
-    @PostMapping("/login")
-    public void loginENSIASt(@RequestBody LoginRequest request){
 
+    //Changed the return type
+    @PostMapping("/login")
+    public String loginENSIASt(@RequestBody LoginRequest request){
+        if(ensiaStService.findByEmail(request.getEmail()).isEmpty()){
+            return String.format("%s : doesn't exist",request.getEmail());
+        }
+        return "Logged successfully";
     }
 
 
