c65da40a8c0c176e60975b7e8ebaf76333689249
Hamza EL MRABTI <elmrabti.hamza@gmail.com>
2023-05-04 00:06:12 +0100

    Link message-member

--- MemberService/src/main/java/com/ensias/model/Member.java
+++ MemberService/src/main/java/com/ensias/model/Member.java
@@ -1,9 +1,12 @@
 package com.ensias.model;
 
+import java.util.Collection;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,10 +19,12 @@ public class Member {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id ;
-	String nom ;
-	String prenom ;
-	String promo ;
-	String filiere ;
+	private String nom ;
+	private String prenom ;
+	private String promo ;
+	private String filiere ;
+	@OneToMany(mappedBy = "member")
+	private Collection<Message> messagesEnvoyes ;
 	
 
 }
--- MessagesService/src/main/java/com/ensias/model/Message.java
+++ MessagesService/src/main/java/com/ensias/model/Message.java
@@ -1,9 +1,13 @@
 package com.ensias.model;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +23,8 @@ public class Message {
 	private String contenue ;
 	private int idSender ;
 	private int idReceiver ;
+	@ManyToOne
+	private Member membre ;
 	
 
 }
